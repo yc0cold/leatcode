c3a530992d57ecaea162e1a434139f3a292f7598
yc0cold <yc0cold@gmail.com>
2022-08-28 18:13:58 +0900

    876. Middle of the Linked List

--- MiddleOfTheLinkedList.java
+++ MiddleOfTheLinkedList.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class MiddleOfTheLinkedList {
+public class MiddleOfTheLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode temp1 = new ListNode();
+        temp1 = head;
+
+        int count = 0;
+        int target = 0;
+
+        // count the length of LinkedList
+        while(temp1 != null){
+            temp1 = temp1.next;
+            count += 1;
+        }
+
+        target = count/2+1;
+
+        for(int i=1; i<target+1; i++){
+            if(target-i==0){
+                return head;
+            }
+            head = head.next;
+        }
+
+        return null;
+    }
 }
+
+
